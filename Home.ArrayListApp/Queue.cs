0233b232dcb3e8a4fb980ba058a094887d5d4430
michael-gungaram <michael.gungaram@gmail.com>
2015-02-17 17:00:57 +0100

    Queue object's code written, but not tested.

--- Home.ArrayListApp/Queue.cs
+++ Home.ArrayListApp/Queue.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * 17/02/2015 : must writted but must be tested with units tests.
+ * 
+ * */
+
+using System;
 
 namespace Home.ArrayListApp
 {
     public class Queue<T>
     {
+        readonly ArrayList<T> _queue;
+        int _counter;
+
+        public Queue()
+        {
+            _queue = new ArrayList<T>( 5 );
+            _counter = 0;
+        }
+
+        public int Count
+        {
+            get { return _queue.Count; }
+        }
+
+        public void Enqueue( T item )
+        {
+            _queue.Add( item );
+            _counter++;
+        }
+
+        public T Dequeue()
+        {
+            T value = _queue[0];
+            _queue.RemoveAt( 0 );
+            return value;
+        }
+
+        public T Peek()
+        {
+            return _queue[0];
+        }
     }
 }
